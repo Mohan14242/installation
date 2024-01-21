cc2f2d87b1e7fedd64dff09bdd24dd2a6d9d308c
mohan <mohan14242@gmail.com>
2024-01-21 11:13:43 +0530

    mohan

--- ansible.sh
+++ ansible.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-my_array=("ansible.yaml" "docker.yaml" "jenkins.yaml" "kubernets.yaml" "terraform.yaml")
+my_array=( "docker.yaml" "jenkins.yaml" "kubernets.yaml" "terraform.yaml")
 for i in "${my_array}";do 
     ansible-playbook -i inventory.ini ${i}
 done
